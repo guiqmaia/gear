a05c7e2a403cc6da22e1f83d71de40fdfa0d50fb
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-12 08:17:29 -0300

    splash

---
+++ lib/presenter/splash/splash_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
