09709a29b22712896844e06f7978cf33c0098268
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-12 08:27:26 -0300

    splash

--- lib/presenter/splash/splash_page.dart
+++ lib/presenter/splash/splash_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:gear/main.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -9,8 +11,22 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+ @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 2)).then((value) => {
+      Navigator.of(context).push(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      ),
+    });
+    super.initState();
+  }
+ 
   @override
   Widget build(BuildContext context) {
-    
+    return Scaffold(
+      body: Center(
+        child: Image.asset(''),
+      ),
+    );
   }
 }
