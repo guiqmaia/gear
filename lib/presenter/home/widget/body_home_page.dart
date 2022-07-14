292d14a30584bb9d2570c82c5f0cb1b3e8c12d13
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-14 10:12:39 -0300

    [sty

--- lib/presenter/home/widget/body_home_page.dart
+++ lib/presenter/home/widget/body_home_page.dart
@@ -1,63 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gear/presenter/product_signup/product_signup_page.dart';
-import 'package:gear/presenter/results/results_page.dart';
 
-import '../cash_register/cash_register_page.dart';
-import '../inventory/inventory_page.dart';
-import '../profile/profile_page.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
-  static const List<Widget> _widgetOptions = <Widget>[
-    BodyHomePage(),
-    BodyResultsPage(),
-    BodyProfilePage(),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(
-      () {
-        _selectedIndex = index;
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.bar_chart),
-            label: 'Resultados',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Perfil',
-            backgroundColor: Colors.purple,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
+import '../../cash_register/cash_register_page.dart';
+import '../../inventory/inventory_page.dart';
+import '../../product_signup/product_signup_page.dart';
 
 class BodyHomePage extends StatelessWidget {
   const BodyHomePage({
