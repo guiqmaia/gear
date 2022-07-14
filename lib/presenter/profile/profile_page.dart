38d5fed0de1b62bf8214ab2f1431aa6f6240112c
Matheus <matheuspitschh@gmail.com>
2022-07-14 10:24:33 -0300

    [change] add SingleChildScrollView in page Profile

--- lib/presenter/profile/profile_page.dart
+++ lib/presenter/profile/profile_page.dart
@@ -26,39 +26,41 @@ class BodyProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const TopBarApp(
-          title: "Perfil",
-          pageRoute: HomePage(),
-          isProfile: false,
-        ),
-        Container(
-          margin: const EdgeInsets.symmetric(vertical: 30),
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            color: Colors.black,
-            borderRadius: BorderRadius.circular(100),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const TopBarApp(
+            title: "Perfil",
+            pageRoute: HomePage(),
+            isProfile: false,
           ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        const Text(
-          "Roberto Silva dos Santos",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25,
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 30),
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.circular(100),
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Container(
-          width: 390,
-          height: 100,
-          color: Colors.white60,
-        )
-      ],
+          const SizedBox(
+            height: 5,
+          ),
+          const Text(
+            "Roberto Silva dos Santos",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            width: 390,
+            height: 100,
+            color: Colors.white60,
+          ),
+        ],
+      ),
     );
   }
 }
