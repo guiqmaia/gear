fcd0476e99c14b54da41de50833cf50259cc5378
Júlia Luciani De Oliveira <Jlia.Oliveira@proway.treina>
2022-07-14 13:11:51 -0300

    login

---
+++ lib/presenter/login/widgets/reset_password.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:gear/presenter/login/login_page.dart';
+
+class NewPassword extends StatelessWidget {
+  const NewPassword({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: GestureDetector(
+          onTap: () => Navigator.pop(context, false),
+          child: const Icon(
+            Icons.menu,
+          ),
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.only(top: 60, left: 40, right: 40),
+        color: Colors.white,
+        child: ListView(
+          children: [
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset('assets/icons/google.png'),
+            ),
+            const SizedBox(height: 50),
+            const Center(
+              child: Text(
+                'Esqueceu sua senha?',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text('Por favor informe o email associado a sua conta e lhe enviaremos um email para a criação de uma nova senha',
+              textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
