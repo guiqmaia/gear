fb98217116af2c261927389d98872a75991f1e14
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-14 10:01:06 -0300

    [change] switching bottom app bar

--- lib/presenter/home/home_page.dart
+++ lib/presenter/home/home_page.dart
@@ -1,76 +1,124 @@
 import 'package:flutter/material.dart';
 import 'package:gear/presenter/product_signup/product_signup_page.dart';
+import 'package:gear/presenter/profile/profile.dart';
 import 'package:gear/presenter/results/results_page.dart';
+import 'package:gear/presenter/shared/widgets/top_bar_app.dart';
 
 import '../cash_register/cash_register_page.dart';
 import '../inventory/inventory_page.dart';
-import '../profile/profile.dart';
-import 'widget/barNavegation.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    BodyHomePage(),
+    BodyResultsPage(),
+    BodyProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(
+      () {
+        _selectedIndex = index;
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('GEAR'),
-        centerTitle: true,
+      body: _widgetOptions.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Resultados',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Perfil',
+            backgroundColor: Colors.purple,
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              height: MediaQuery.of(context).size.height * 0.25,
-              color: Colors.blue,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const CashRegisterPage(),
-                    ),
-                  );
-                },
-                child: const Text('Registro de Caixa'),
-              ),
+    );
+  }
+}
+
+class BodyHomePage extends StatelessWidget {
+  const BodyHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: MediaQuery.of(context).size.height * 0.25,
+            color: Colors.blue,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const CashRegisterPage(),
+                  ),
+                );
+              },
+              child: const Text('Registro de Caixa'),
             ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              height: MediaQuery.of(context).size.height * 0.25,
-              color: Colors.blue,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const InventoryPage(),
-                    ),
-                  );
-                },
-                child: const Text('Gerenciamento de Estoque'),
-              ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: MediaQuery.of(context).size.height * 0.25,
+            color: Colors.blue,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const InventoryPage(),
+                  ),
+                );
+              },
+              child: const Text('Gerenciamento de Estoque'),
             ),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.85,
-              height: MediaQuery.of(context).size.height * 0.25,
-              color: Colors.blue,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const ProductSignupPage(),
-                    ),
-                  );
-                },
-                child: const Text('Cadastro de Produto'),
-              ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.85,
+            height: MediaQuery.of(context).size.height * 0.25,
+            color: Colors.blue,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const ProductSignupPage(),
+                  ),
+                );
+              },
+              child: const Text('Cadastro de Produto'),
             ),
-            SizedBox(height: MediaQuery.of(context).size.height * 0.02),
-          ],
-        ),
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.02),
+        ],
       ),
-      bottomNavigationBar: BarNavegation(context),
     );
   }
 }
