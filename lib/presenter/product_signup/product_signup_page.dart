d2549920eb0e0095b6eaf1f734142d17e907a1a7
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-14 09:06:41 -0300

    [sty] padronizing

--- lib/presenter/product_signup/product_signup_page.dart
+++ lib/presenter/product_signup/product_signup_page.dart
@@ -1,53 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:gear/presenter/home/home_page.dart';
-import 'package:gear/presenter/shared/widgets/top_bar_app.dart';
 
-import 'Widgets/gradient_form_button.dart';
-import 'Widgets/text_form_signup.dart';
+import 'Widgets/product_signup_body.dart';
 
 class ProductSignupPage extends StatelessWidget {
   const ProductSignupPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Column(
-        children: [
-          const TopBarApp(
-            title: 'Cadastrar Produto',
-            pageRoute: HomePage(),
-            isProfile: true,
-          ),
-          Expanded(
-            child: Stack(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  color: Colors.white,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: ListView(
-                    physics: const BouncingScrollPhysics(),
-                    padding: const EdgeInsets.only(
-                      bottom: 30,
-                      right: 50,
-                      left: 50,
-                    ),
-                    children: const [
-                      TextFormSignup(label: 'Nome'),
-                      TextFormSignup(label: 'Preço'),
-                      TextFormSignup(label: 'Categoria'),
-                      GradientFormButton(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
+    return const Scaffold(
+      body: SignupPageBody(),
     );
   }
 }
