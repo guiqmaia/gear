86cce2eac34e459567ae9a18bede1cb14803d4b9
Guilherme Quirino Maia <Guilherme.Maia@proway.treina>
2022-07-19 13:44:34 -0300

    fix bug in signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gear/presenter/home/home_page.dart';
-import 'package:gear/presenter/login/login_page.dart';
-import 'package:gear/presenter/product_signup/product_signup_page.dart';
 
-import 'presenter/splash/splash_page.dart';
+import 'presenter/login/login_page.dart';
 
 void main(List<String> args) {
   runApp(
