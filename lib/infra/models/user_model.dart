21166df5cc70aff897f9884a8517f1ad01f49251
Luan Gustavo Fortunato <Luan.Fortunato@proway.treina>
2022-07-19 16:46:55 -0300

    [fix] fix product list not appearing

--- lib/infra/models/user_model.dart
+++ lib/infra/models/user_model.dart
@@ -1,40 +1,58 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
-import 'package:gear/infra/models/company_model.dart';
-
 class UserModel {
   String name;
-  String cpf;
-  String phone;
-  String email;
-  CompanyModel company;
+  String birthday;
+  String company;
+  String cnpj;
+  String telephone;
+  String cellPhone;
+  String cep;
+  String adress;
+  String login;
+  String password;
 
   UserModel({
     required this.name,
-    required this.cpf,
-    required this.phone,
-    required this.email,
+    required this.birthday,
     required this.company,
+    required this.cnpj,
+    required this.telephone,
+    required this.cellPhone,
+    required this.cep,
+    required this.adress,
+    required this.login,
+    required this.password,
   });
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'name': name,
-      'cpf': cpf,
-      'phone': phone,
-      'email': email,
-      'company': company.toMap(),
+      'birthday': birthday,
+      'company': company,
+      'cnpj': cnpj,
+      'telephone': telephone,
+      'mobileNumber': cellPhone,
+      'cep': cep,
+      'adress': adress,
+      'email': login,
+      'password': password,
     };
   }
 
   factory UserModel.fromMap(Map<String, dynamic> map) {
     return UserModel(
       name: map['name'] as String,
-      cpf: map['cpf'] as String,
-      phone: map['phone'] as String,
-      email: map['email'] as String,
-      company: CompanyModel.fromMap(map['company'] as Map<String, dynamic>),
+      birthday: map['birthday'] as String,
+      company: map['company'] as String,
+      cnpj: map['cnpj'] as String,
+      telephone: map['telephone'] as String,
+      cellPhone: map['mobileNumber'] as String,
+      cep: map['cep'] as String,
+      adress: map['adress'] as String,
+      login: map['email'] as String,
+      password: map['password'] as String,
     );
   }
 
