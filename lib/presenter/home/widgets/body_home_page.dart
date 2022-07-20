7d1d224f4232fd159ae78431d971befbf1a2897f
Julia <Jlia.Oliveira@proway.treina>
2022-07-20 08:06:55 -0300

    home

--- lib/presenter/home/widgets/body_home_page.dart
+++ lib/presenter/home/widgets/body_home_page.dart
@@ -60,7 +60,7 @@ class _BodyHomePageState extends State<BodyHomePage> {
                   Padding(
                     padding: EdgeInsets.only(top: 5, bottom: 5),
                     child: Text(
-                      'Drinks && Company',
+                      'Distribuidora Bebidinhas',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 20,
