b9b6c531444d02c9c2caeacb804f12831ecdeae5
Julia <Jlia.Oliveira@proway.treina>
2022-07-20 16:52:51 -0300

    home

--- lib/presenter/home/widgets/body_home_page.dart
+++ lib/presenter/home/widgets/body_home_page.dart
@@ -3,14 +3,11 @@ import 'package:flutter/material.dart';
 import '../../shared/widgets/btn_standard_app.dart';
 
 import '../../../core/app_assets.dart';
-import 'package:onboarding/onboarding.dart';
 
 import '../../../infra/database/create_database_products.dart';
 import '../../cash_register/cash_register_page.dart';
 import '../../category/category_page.dart';
 
-import 'column_profile_info.dart';
-import 'decorated_box_chart_results.dart';
 import 'top_bar_home.dart';
 
 class BodyHomePage extends StatefulWidget {
@@ -24,23 +21,6 @@ class _BodyHomePageState extends State<BodyHomePage> {
   static bool isCreated = false;
   late Material materialButton;
   late int index;
-  final onboardingPagesList = [
-    PageModel(
-      widget: DecoratedBox(
-        decoration: BoxDecoration(
-          color: backgroundGrey,
-          border: Border.all(
-            width: 0,
-            color: backgroundGrey,
-          ),
-        ),
-        child: const ColumnProfileInfo(),
-      ),
-    ),
-    PageModel(
-      widget: const DecoratedBoxChartResults(),
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -52,41 +32,68 @@ class _BodyHomePageState extends State<BodyHomePage> {
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                const TopBarHome(),
-                Container(
-                  alignment: Alignment.center,
-                  color: Colors.grey.shade400,
-                  width: double.infinity,
-                  height: 350,
-                  child: Onboarding(
-                    pages: onboardingPagesList,
-                    onPageChange: (int pageIndex) {
-                      index = pageIndex;
-                    },
-                    startPageIndex: 0,
-                    footerBuilder:
-                        (context, dragDistance, pagesLength, setIndex) {
-                      return Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 35, 0),
-                        child: CustomIndicator(
-                          netDragPercent: dragDistance,
-                          pagesLength: pagesLength,
-                          indicator: Indicator(
-                            activeIndicator: const ActiveIndicator(
-                              color: Colors.black,
-                              borderWidth: 1.4,
-                            ),
-                            closedIndicator: ClosedIndicator(color: greenNeon),
-                            indicatorDesign: IndicatorDesign.polygon(
-                              polygonDesign: PolygonDesign(
-                                polygon: DesignType.polygon_circle,
+                Stack(
+                  children: [
+                    Container(
+                      height: 400,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(150),
+                          bottomRight: Radius.circular(150),
+                        ),
+                      ),
+                      child: Column(children: [
+                        const Text('Gear'),
+                        CircleAvatar(
+                          radius: 80,
+                          backgroundImage: Image.asset(
+                            imgStore,
+                            fit: BoxFit.cover,
+                          ).image,
+                        ),
+                      ]),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        padding: const EdgeInsets.only(top: 10, bottom: 10),
+                        
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: greenNeon,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Padding(
+                              padding: EdgeInsets.only(top: 5, bottom: 5),
+                              child: Text(
+                                'Distribuidora Bebidinhas',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
-                          ),
+                            Text(
+                              '27.042.017/00001-22',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 15),
+                            ),
+                            Text('Douglas Costa da Silva',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 15,
+                                )),
+                          ],
                         ),
-                      );
-                    },
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
                   height: 30,
