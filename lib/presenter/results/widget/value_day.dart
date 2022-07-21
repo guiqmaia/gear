e0b53f32fd92bf27b0816b0a2d461e92fe72a157
Matheus <matheuspitschh@gmail.com>
2022-07-21 08:35:59 -0300

    [change] final page results

--- lib/presenter/results/widget/value_day.dart
+++ lib/presenter/results/widget/value_day.dart
@@ -12,46 +12,46 @@ class ValueDay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(padding: EdgeInsets.fromLTRB(50, 10, 50, 0)),
-            Text(
-              day,
-              style: const TextStyle(
-                color: Colors.black,
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-        Column(
-          children: [
-            const Padding(padding: const EdgeInsets.fromLTRB(50, 10, 50, 0)),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: greenNeon,
-              ),
-              child: Text(
-                value,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 38, ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                day,
                 style: const TextStyle(
                   color: Colors.black,
                   fontSize: 17,
                   fontWeight: FontWeight.bold,
                 ),
-                
+                textAlign: TextAlign.center,
               ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+          Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: greenNeon,
+                ),
+                child: Text(
+                  value,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
