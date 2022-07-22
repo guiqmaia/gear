f530f8dc310f487c29fb702783e6086e4cf7362a
Anna Cecília Roncalio <Anna.Roncalio@proway.treina>
2022-07-22 15:48:10 -0300

    [feat] add refactor edit product

--- lib/presenter/edit_product/edit_product_page.dart
+++ lib/presenter/edit_product/edit_product_page.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:gear/presenter/category/category_page.dart';
-import 'package:image_picker/image_picker.dart';
 
 import '../../core/app_assets.dart';
-import '../../infra/database/gear_database.dart';
-import '../../shared/widgets/text_field_app.dart';
-import '../../shared/widgets/top_bar_app.dart';
-import '../product/product_page.dart';
-import '../product_signup/Widgets/default_image_container.dart';
+import 'widgets/body_edit_product.dart';
 
-class EditProductPage extends StatefulWidget {
+class EditProductPage extends StatelessWidget {
   final int productCode;
   final String categoryTitle;
 
@@ -27,167 +17,13 @@ class EditProductPage extends StatefulWidget {
   }) : super(key: key);
 
   @override
-  State<EditProductPage> createState() => _EditProductPageState();
-}
-
-class _EditProductPageState extends State<EditProductPage> {
-  TextEditingController newNameController = TextEditingController();
-  TextEditingController newPriceController = TextEditingController();
-  TextEditingController newQuantityController = TextEditingController();
-
-  Uint8List? photo;
-  File? img;
-
-  Future pickImage() async {
-    try {
-      final img = await ImagePicker().pickImage(source: ImageSource.gallery);
-      if (img == null) return;
-      final imageTemp = File(img.path);
-      setState(() {
-        this.img = imageTemp;
-        photo = imageTemp.readAsBytesSync();
-      });
-    } on PlatformException catch (e) {
-      // ignore: avoid_print
-      print('Failed to pick image: $e');
-    }
-  }
-
-  Future updateNameProduct() async {
-    if (newNameController.text != '') {
-      await GearDatabase.instance.updateProduct(
-        widget.productCode,
-        'name',
-        '"${newNameController.text}"',
-      );
-    }
-  }
-
-  Future updatePriceProduct() async {
-    if (newPriceController.text != '') {
-      await GearDatabase.instance.updateProduct(
-        widget.productCode,
-        'price',
-        double.parse(newPriceController.text),
-      );
-    }
-  }
-
-  Future updateQuantityProduct() async {
-    if (newQuantityController.text != '') {
-      await GearDatabase.instance.updateProduct(
-        widget.productCode,
-        'quantity',
-        int.parse(newQuantityController.text),
-      );
-    }
-  }
-
-  Future updateImgProduct() async {
-    if (img != null) {
-      await GearDatabase.instance.updateProduct(
-        widget.productCode,
-        'image',
-        photo,
-      );
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundGrey,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const TopBarApp(
-              title: 'Editar produto',
-              pageRoute: null,
-              isProfile: false,
-            ),
-            TextFieldApp(
-              labelItem: 'Nome',
-              typeController: newNameController,
-              isObscured: false,
-            ),
-            TextFieldApp(
-              labelItem: 'Preço',
-              typeController: newPriceController,
-              isObscured: false,
-            ),
-            TextFieldApp(
-              labelItem: 'Quantidade',
-              typeController: newQuantityController,
-              isObscured: false,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: greenNeon,
-              ),
-              margin: const EdgeInsets.all(15),
-              padding: const EdgeInsets.symmetric(
-                vertical: 3,
-              ),
-              width: MediaQuery.of(context).size.width * 0.9,
-              child: TextButton(
-                onPressed: (() => pickImage()),
-                child: const Text(
-                  'Alterar imagem',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.85,
-              child: img != null
-                  ? Image.memory(photo!)
-                  : const DefaulImageContainer(),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: greenNeon,
-              ),
-              margin: const EdgeInsets.all(15),
-              width: MediaQuery.of(context).size.width * 0.9,
-              padding: const EdgeInsets.symmetric(
-                vertical: 3,
-              ),
-              child: TextButton(
-                onPressed: () async {
-                  updateNameProduct();
-                  updatePriceProduct();
-                  updateQuantityProduct();
-                  updateImgProduct();
-
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const CategoryPage(),
-                    ),
-                  );
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ProductPage(
-                        categoryTitle: widget.categoryTitle,
-                      ),
-                    ),
-                  );
-                },
-                child: const Text(
-                  'Editar',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        child: BodyEditProduct(
+          categoryTitle: categoryTitle,
+          productCode: productCode,
         ),
       ),
     );
---
+++ lib/presenter/edit_product/widgets/btn_edit_product.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+import '../../../core/app_assets.dart';
+import '../../../infra/database/gear_database.dart';
+import '../../category/category_page.dart';
+import '../../product/product_page.dart';
+
+class BtnEditProduct extends StatefulWidget {
+  const BtnEditProduct({
+    Key? key,
+    required this.newNameController,
+    required this.newPriceController,
+    required this.newQuantityController,
+    required this.productCode,
+    required this.categoryTitle,
+    //required this.photo,
+  }) : super(key: key);
+
+  final TextEditingController newNameController;
+  final TextEditingController newPriceController;
+  final TextEditingController newQuantityController;
+  final int productCode;
+  final String categoryTitle;
+  //Uint8List? photo;
+
+  @override
+  State<BtnEditProduct> createState() => _BtnEditProductState();
+}
+
+class _BtnEditProductState extends State<BtnEditProduct> {
+  Future updateNameProduct() async {
+    if (widget.newNameController.text != '') {
+      await GearDatabase.instance.updateProduct(
+        widget.productCode,
+        'name',
+        '"${widget.newNameController.text}"',
+      );
+    }
+  }
+
+  Future updatePriceProduct() async {
+    if (widget.newPriceController.text != '') {
+      await GearDatabase.instance.updateProduct(
+        widget.productCode,
+        'price',
+        double.parse(widget.newPriceController.text),
+      );
+    }
+  }
+
+  Future updateQuantityProduct() async {
+    if (widget.newQuantityController.text != '') {
+      await GearDatabase.instance.updateProduct(
+        widget.productCode,
+        'quantity',
+        int.parse(widget.newQuantityController.text),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: greenNeon,
+      ),
+      margin: const EdgeInsets.all(15),
+      width: MediaQuery.of(context).size.width * 0.9,
+      padding: const EdgeInsets.symmetric(
+        vertical: 3,
+      ),
+      child: TextButton(
+        onPressed: () {
+          updateNameProduct();
+          updatePriceProduct();
+          updateQuantityProduct();
+
+          Navigator.of(context).pop();
+          Navigator.of(context).pop();
+        },
+        child: const Text(
+          'Editar',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
